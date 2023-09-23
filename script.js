943344431d3096df38f1ecd37f8fb3a1b1bdaacc
Oliver Richardson <123179922+Olliekse@users.noreply.github.com>
2023-09-23 21:38:59 +0300

    created grid size button

---
+++ script.js
@@ -0,0 +1,35 @@
+const btn = document.querySelector(".btn");
+const divContainer = document.querySelector(".div-container");
+let numTiles = 16;
+
+const loadGrid = () => {
+  const containerSize = 450;
+  const totalBoxes = numTiles * numTiles;
+  const boxSize = containerSize / numTiles;
+
+  for (i = 0; i < totalBoxes; i++) {
+    const divEl = document.createElement("div");
+    divEl.classList.add("div");
+    divEl.style.width = boxSize - 1 + "px";
+    divEl.style.height = boxSize - 1 + "px";
+    divContainer.appendChild(divEl);
+  }
+};
+
+const init = () => {
+  loadGrid();
+};
+init();
+
+const removeTiles = () => {
+  divContainer.innerHTML = "";
+};
+
+btn.addEventListener("click", () => {
+  numTiles = Number(prompt("How many tiles do you want?"));
+  if (numTiles > 100) {
+    numTiles = 100;
+  }
+  removeTiles();
+  loadGrid();
+});
